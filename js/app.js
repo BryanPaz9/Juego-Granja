61c357465d2166b25ef9512caa0c16f337f5c8b7
Bryan de Paz <bryanascuc7@gmail.com>
2018-10-16 15:51:30 -0600

    Dibujando objetos en el tablero

--- js/app.js
+++ js/app.js
@@ -7,14 +7,40 @@ var fondo = {
 
 };
 
-fondo.imagen = new Image();
-fondo.imagen.src = fondo.url;
-fondo.imagen.addEventListener("load",function(){
+var vaca ={
+    url: './imagenes/vaca.png',
+    imagen: Image
+}
 
+var cerdo = {
+    url: './imagenes/cerdo.png',
+    imagen: Image
+
+}
+
+fondo.imagen = new Image();
+vaca.imagen = new Image();  
+cerdo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+vaca.imagen.src = vaca.url;
+cerdo.imagen.src = cerdo.url;
+
+fondo.imagen.addEventListener("load",function(){
+    // lapiz.drawImage(fondo.imagen,0,0);
     dibujar();
 });
+vaca.imagen.addEventListener("load",function(){
+    dibujar();
+    // lapiz.drawImage(vaca.imagen,150,100);
+})
+cerdo.imagen.addEventListener("load",function(){
+    dibujar();
+
+})
 
 function dibujar(){
 lapiz.drawImage(fondo.imagen,0,0);
+lapiz.drawImage(vaca.imagen,150,100);
+lapiz.drawImage(cerdo.imagen,300,200);
 }
 
