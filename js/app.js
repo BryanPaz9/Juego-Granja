ac9c1a262fd5a95068760d3cdbcce6bdc146f27f
Bryan de Paz <bryanascuc7@gmail.com>
2018-10-17 14:59:57 -0600

    Matriz con animales aleatorios

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
+document.addEventListener("keydown",movimiento);
 var canvas = document.getElementById('fondo');
 var lapiz = canvas.getContext('2d');
+var x= random(0,5)*80;
+var y = random(0,5)*80;
+var y1 = 0;
+var y2 = 0;
+var x1 = 0;
+var x2 =0;
+const DIMENSION = 80;
+var randomx = random(0,5);
+var randomy = random(0,5);
+var randomy1 = random(0,5);
+var randomx1 = random(0,5);
+randomC = random(1,6);
+randomV = random(1,6);
+var matriz = new Array(6);  
 
 var fondo = {
     url: './imagenes/tile.png',
@@ -41,31 +56,130 @@ fondo.imagen.addEventListener("load",function(){
 });
 vaca.imagen.addEventListener("load",function(){
     vaca.cargaOk = true;
-    dibujar();
+     dibujar();
 });
 cerdo.imagen.addEventListener("load",function(){
     cerdo.cargaOk = true;
-    dibujar();
+     dibujar();
 });
 cuchillo.imagen.addEventListener("load",function(){
     cuchillo.cargaOk = true;
     dibujar();
 });
+iniciarMatriz();
+inicializarCerdo();
+inicializarVacas();
+dibujar();
+
+var tecla ={
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+    ENTER: 13
+
+}
 
 function dibujar(){
-    if(fondo.cargaOk == true){
+    if(fondo.cargaOk){
         lapiz.drawImage(fondo.imagen,0,0);
-
     }
-    if(vaca.cargaOk == true){
-
-        lapiz.drawImage(vaca.imagen,150,100);
+    // if(vaca.cargaOk == true){
+    //    x1 = randomx*DIMENSION;
+    //    y1 = randomy*DIMENSION;
+    //     lapiz.drawImage(vaca.imagen,x1,y1);
+    // }
+    // if(cerdo.cargaOk == true){
+    //     x2 = randomx1*DIMENSION;
+    //     y2 = randomy1*DIMENSION;
+    //     lapiz.drawImage(cerdo.imagen,x2,y2);  
+    // }
+    dibujarMatriz();
+     if(cuchillo.cargaOk){
+        lapiz.drawImage(cuchillo.imagen,x,y);
     }
-    if(cerdo.cargaOk == true){
-        lapiz.drawImage(cerdo.imagen,300,200);  
-    }
-    if(cuchillo.cargaOk == true){
-        lapiz.drawImage(cuchillo.imagen,250,400);
+}
+function movimiento(evento){
+    
+    // console.log(evento);
+    switch(evento.keyCode){
+        case tecla.LEFT:
+            // alert("Izquierda");
+            if(x!=0){
+            x-= DIMENSION;
+            dibujar();
+            }
+            break;
+        case tecla.UP:
+        if(y !=0){
+            y-= DIMENSION;
+            dibujar();
+        }
+            break;
+        case tecla.RIGHT:
+        if (x !=400){
+            x+= DIMENSION;
+            dibujar();
+        }
+            break;
+        case tecla.DOWN:
+        if(y !=400){
+            y+= DIMENSION;
+            dibujar();
+        }
+            break;
+        case tecla.ENTER:   
+        console.log(matriz);
+        // alert(matriz[2]);
+        break;
     }
 }
 
+function random(minimo, maximo){
+    return Math.floor(Math.random()*(maximo -minimo+1 ))+minimo;
+
+}
+
+function iniciarMatriz(){
+    for (var i = 0; i <matriz.length; i++){
+        matriz[i] = new Array(6);
+        for(var j = 0; j<matriz.length; j++){
+            matriz[i][j]= 'x';
+        }
+    }
+}
+
+function inicializarVacas(){
+    var numero = random(0,5);
+    for(var i = 0; i<= numero;i++){
+        var fila = random(0,5);
+        var columna = random(0,5);
+        if(matriz[fila][columna] == 'x'){
+            matriz[fila][columna] ='v';
+        }
+    }
+}
+
+function inicializarCerdo(){
+    var numero = random(0,5);
+    for(var i = 0; i<= numero; i++){
+        var fila = random(0,5);
+        var columna = random(0,5);
+        if(matriz[fila][columna]== 'x'){
+            matriz[fila][columna] = 'c'; 
+        }
+    }
+}
+
+function dibujarMatriz(){
+    for(var i = 0; i<matriz.length; i++){
+        for(var j =0; j<matriz.length;j++){
+            if(matriz[i][j]=='v'){
+                // lapiz.drawImage(vaca.imagen ,i*DIMENSION, j*DIMENSION);
+                lapiz.drawImage(vaca.imagen,i*80,j*80);
+            }else if(matriz[i][j]=='c'){
+                lapiz.drawImage(cerdo.imagen,i*DIMENSION,j*DIMENSION);
+            }
+        }
+    }
+}
